86f6330b163cf69b2e04e3b671a1119231edb5ef
Konstantin <konstantin.boboshko@gmail.com>
2023-11-29 17:22:11 +0300

    created a list of methods to work with at database class

--- database.py
+++ database.py
@@ -34,6 +34,16 @@ class DatabaseCon:
         cursor.execute(select_query)
         return cursor.fetchall()
 
+    def save_object(self, insert_query):
+        cursor = self.get_cursor()
+        cursor.execute(insert_query)
+        self.close()
+
+    def get_user_id(self, discord_id):
+        cursor = self.get_cursor()
+        cursor.execute(f"SELECT id FROM users WHERE discord_id = '{discord_id}'")
+        return cursor.fetchone()
+
     def get_users(self):
         return self.get_objects(f'SELECT * FROM users')
 
@@ -43,10 +53,28 @@ class DatabaseCon:
     def get_user_keywords(self, user_id):
         return self.get_objects(f"SELECT key_string FROM keywords WHERE user_id = '{user_id}'")
 
-    def save_image(self, created_at, guild_id, channel_id, image_url):
-        cursor = self.get_cursor()
-        cursor.execute(f'''
-        INSERT INTO drops (created_at, guild_id, channel_id, image_url) 
-        VALUES('{created_at}', '{guild_id}', '{channel_id}', '{image_url}')
+    def save_user(self, discord_id, username):
+        self.save_object(f'''
+            INSERT INTO users (discord_id, username) 
+            VALUES('{discord_id}', '{username}')
         ''')
-        self.close()
+
+    def save_keyword(self, discord_id, keyword):
+        user_id = self.get_user_id(discord_id)[0]
+        self.save_object(f'''
+            INSERT INTO keywords (user_id, key_string) 
+            VALUES('{user_id}', '{keyword}')
+        ''')
+
+    def delete_keyword(self, discord_id, keyword):
+        user_id = self.get_user_id(discord_id)[0]
+        self.save_object(f'''
+            DELETE FROM keywords  
+            WHERE user_id = '{user_id}' AND key_string = '{keyword}'
+        ''')
+
+    def save_image(self, created_at, guild_id, channel_id, image_url):
+        self.save_object(f'''
+            INSERT INTO drops (created_at, guild_id, channel_id, image_url) 
+            VALUES('{created_at}', '{guild_id}', '{channel_id}', '{image_url}')
+        ''')
